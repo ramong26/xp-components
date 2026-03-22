3595ef5649d2f1907a48f63adad012ab1b2d6a80
ramong26 <rlatndus21049@gmail.com>
2026-03-22 10:21:30 +0900

    Fix component named exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,26 @@
-export * as Button from './Button/Button';
-export * as Carousel from './Carousel/Carousel';
-export * as Checkbox from './Checkbox/Checkbox';
-export * as Chips from './Chips/Chips';
-export * as Input from './Input/Input';
-export * as Select from './Select/Select';
-export * as Slider from './Slider/Slider';
-export * as Switch from './Switch/Switch';
-export * as Tag from './Tag/Tag';
+export { default as Button } from './Button/Button';
+export type { ButtonProps, Variant as ButtonVariant } from './Button/Button';
+
+export { default as Carousel } from './Carousel/Carousel';
+export type { CarouselItem, CarouselProps } from './Carousel/Carousel';
+
+export { default as Checkbox } from './Checkbox/Checkbox';
+export type { CheckboxProps } from './Checkbox/Checkbox';
+
+export { default as Chips } from './Chips/Chips';
+export type { ChipsProps } from './Chips/Chips';
+
+export { default as Input } from './Input/Input';
+export type { InputProps, Variant as InputVariant } from './Input/Input';
+
+export { default as Select } from './Select/Select';
+export type { SelectProps } from './Select/Select';
+
+export { default as Slider } from './Slider/Slider';
+export type { SliderProps } from './Slider/Slider';
+
+export { default as Switch } from './Switch/Switch';
+export type { SwitchProps } from './Switch/Switch';
+
+export { default as Tag } from './Tag/Tag';
+export type { Size as TagSize, TagProps, Variant as TagVariant } from './Tag/Tag';
